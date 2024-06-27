5ef1ce5e0a9cd5860904c8ac6107d4ca53fdf070
DUDEbehindDUDE <jnbrooks457@gmail.com>
2024-06-27 16:46:57 -0400

    basic test with react query connecting to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,53 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      a
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Data />
+      </div>
+    </QueryClientProvider>
+  );
+}
+
+function Data() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["test"],
+    queryFn: () =>
+      fetch(
+        "http://localhost:5096/api/OccupancyData/currentoccupancydata"
+      ).then((res) => res.json()),
+    refetchInterval: 1000
+  });
+
+  if (isPending) return <p>"Loading..."</p>;
+  if (error) return <p>"Error! " + error.message</p>;
+
+  const lastUpdate = new Date(data.time);
+  const dining = data.data.dining;
+
+  return (
+    <>
+      <p>
+        Social 704: {dining.current704}/{dining.max704} ({dining.percent704}%
+        full)
+      </p>
+      <p>
+        Social Village: {dining.currentSoVi}/{dining.maxSoVi} (
+        {dining.percentSoVi}% full)
+      </p>
+      <p>
+        Last Updated: {lastUpdate.getHours()}:{lastUpdate.getMinutes()}:{lastUpdate.getSeconds()}
+      </p>
+    </>
   );
 }
 
