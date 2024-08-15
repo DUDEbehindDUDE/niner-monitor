7609cfa6e98cb2748fe360ef4031cc98b43beb03
DUDEbehindDUDE <66212375+DUDEbehindDUDE@users.noreply.github.com>
2024-08-15 12:57:11 -0400

    Add bold variant to InfoQuestionText

--- src/Components/InfoQuestionText.tsx
+++ src/Components/InfoQuestionText.tsx
@@ -5,15 +5,22 @@ function InfoQuestionText({
   text,
   tooltip,
   sx,
+  bold = false,
 }: {
   text: string;
   tooltip: string;
   sx?: SxProps;
+  bold?: boolean;
 }) {
+  const variant = bold ? "body1" : "body2";
+  const weight = bold ? "bold" : null;
   return (
     <Box sx={sx}>
       <Tooltip title={tooltip}>
-        <Typography variant="body2" sx={{ mt: 1, width: "fit-content" }}>
+        <Typography
+          variant={variant}
+          sx={{ mt: 1, width: "fit-content", fontWeight: weight }}
+        >
           {text}
           <HelpOutline
             sx={{ fontSize: "1.2em", verticalAlign: "-16%", ml: 1 }}
