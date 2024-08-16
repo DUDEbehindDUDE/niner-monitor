496d36365bf6b466a9d8c3bc3fb2c7b798dc8eff
DUDEbehindDUDE <jnbrooks457@gmail.com>
2024-08-15 22:18:54 -0400

    Add favorites to parking lots
    Currently will reset on refresh, will add cookies later

--- src/Components/ParkingData.tsx
+++ src/Components/ParkingData.tsx
@@ -3,17 +3,88 @@ import {
   AccordionDetails,
   AccordionSummary,
   Box,
+  Button,
+  FormControlLabel,
+  FormGroup,
+  Grid,
+  IconButton,
   Paper,
+  Switch,
   Typography,
 } from "@mui/material";
 import { OccupancyParking } from "../types/OccupancyResponseData";
 import { display } from "../util/util";
-import { ExpandMore } from "@mui/icons-material";
+import { ExpandMore, Favorite, Star } from "@mui/icons-material";
 import HistoricalItemGraph from "./HistoricalItemGraph";
 import InfoQuestionText from "./InfoQuestionText";
+import { useState } from "react";
 
 function ParkingData({ parking }: { parking: OccupancyParking[] }) {
+  const [starred, setStarred] = useState<string[]>(["South Village Deck"]);
+  const [hideNonStars, setHideNonStars] = useState(false);
   const bg = "rgba(255, 255, 255, 0.04)";
+
+  let normalLotItems: JSX.Element[] = [];
+  let starredLotItems: JSX.Element[] = [];
+  for (const item of parking) {
+    const _starred = starred.includes(item.name);
+    const parkingElement: JSX.Element = (
+      <>
+        <Box display={"flex"} justifyContent="space-between">
+          <Typography sx={{ fontWeight: "bold", mt: 1 }}>
+            {item.name}
+          </Typography>
+          <IconButton
+            color={_starred ? "secondary" : "default"}
+            onClick={() => editStar(item.name, _starred)}
+          >
+            <Star />
+          </IconButton>
+        </Box>
+        <Box sx={{ mt: 1 }}>
+          <Accordion id={item.name}>
+            <AccordionSummary expandIcon={<ExpandMore />} sx={{ bgcolor: bg }}>
+              {display(1 - item.percentAvailable)}
+            </AccordionSummary>
+            <AccordionDetails sx={{ bgcolor: bg }}>
+              <HistoricalItemGraph type={"parking"} subtype={item.name} />
+            </AccordionDetails>
+          </Accordion>
+        </Box>
+      </>
+    );
+    if (_starred) {
+      starredLotItems = [...starredLotItems, parkingElement];
+    } else if (!hideNonStars) {
+      normalLotItems = [...normalLotItems, parkingElement];
+    }
+  }
+  const parkingItems = [...starredLotItems, ...normalLotItems];
+  console.log(parkingItems);
+
+  function editStar(item: string, current: boolean) {
+    if (!current) {
+      setStarred((prev) => [...prev, item]);
+      return;
+    }
+
+    setStarred((prev) => {
+      let newStarred: string[] = [];
+      for (const str of prev) {
+        if (str !== item) {
+          newStarred = [...newStarred, str];
+        }
+      }
+      return newStarred;
+    });
+  }
+
+  function handleHideNonStarsChange(
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {
+    setHideNonStars(event.target.checked);
+  }
+
   return (
     <Paper style={{ padding: 20 }}>
       <Typography variant="h6">Parking Data</Typography>
@@ -22,26 +93,13 @@ function ParkingData({ parking }: { parking: OccupancyParking[] }) {
         tooltip="Lot capacity is updated realtime, as long as you have an internet connection"
         sx={{ mb: 2 }}
       />
-      {parking.map((item) => (
-        <>
-          <Typography sx={{ fontWeight: "bold", mt: 1 }}>
-            {item.name}
-          </Typography>
-          <Box sx={{ mt: 1 }}>
-            <Accordion id={item.name}>
-              <AccordionSummary
-                expandIcon={<ExpandMore />}
-                sx={{ bgcolor: bg }}
-              >
-                {display(1 - item.percentAvailable)}
-              </AccordionSummary>
-              <AccordionDetails sx={{ bgcolor: bg }}>
-                <HistoricalItemGraph type={"parking"} subtype={item.name} />
-              </AccordionDetails>
-            </Accordion>
-          </Box>
-        </>
-      ))}
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch onChange={handleHideNonStarsChange} />}
+          label={"Hide non-starred"}
+        />
+      </FormGroup>
+      {parkingItems}
     </Paper>
   );
 }
